38724253e9b5e783048d691168d29c7564d536d2
Арина Андреевна <Arina.Sheshukova@urfu.me>
2022-09-20 19:45:35 +0500

    Доработаны фунцкии поиска рандомного числа и проверки размера сообщения

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
 function GetRandomNumber (begin, end){
   if(begin >= 0 && end >= begin){
     return Math.round(Math.random() * (end - begin) + begin);
-  } else {return -1;}
+  }
+  return new Error('Error. Change input values');
 }
 GetRandomNumber();
 
 function CalculateMessage(message, maxLength){
-  if (message.Length <= maxLength) {
-    return true;
-  }
-  return false;
+  return(message.Length <= maxLength);
 }
 CalculateMessage();
+
+
